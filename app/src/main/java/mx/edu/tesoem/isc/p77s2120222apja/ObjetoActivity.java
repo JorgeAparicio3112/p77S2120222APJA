9d991a0c3d5fc35210faa67298c3b03c055cf80d
JorgeAparicio3112 <revenant2314@gmail.com>
2022-09-28 11:31:04 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/p77s2120222apja/ObjetoActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.p77s2120222apja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObjetoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objeto);
+    }
+}
